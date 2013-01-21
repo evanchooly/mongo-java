24f30de599d995cdab2f71573a7c5734f1681960
evanchooly <jlee@antwerkz.com>
2013-01-21 16:11:17 -0500

    add critter files

---
+++ src/main/java/com/antwerkz/jfokus/mongo/model/criteria/AddressCriteria.java
@@ -0,0 +1,52 @@
+package com.antwerkz.jfokus.mongo.model.criteria;
+
+import com.antwerkz.critter.TypeSafeFieldEnd;
+import com.google.code.morphia.Datastore;
+import com.google.code.morphia.query.Criteria;
+import com.google.code.morphia.query.CriteriaContainer;
+import com.google.code.morphia.query.Query;
+import org.bson.types.ObjectId;
+
+public class AddressCriteria {
+  private Query<com.antwerkz.jfokus.mongo.model.Address> query;
+
+  public Query<com.antwerkz.jfokus.mongo.model.Address> query() {
+    return query;
+  }
+
+  public AddressCriteria(Datastore ds) {
+    query = ds.find(com.antwerkz.jfokus.mongo.model.Address.class);
+  }
+
+  public CriteriaContainer or(Criteria... criteria) {
+    return query.or(criteria);
+  }
+
+  public CriteriaContainer and(Criteria... criteria) {
+    return query.and(criteria);
+  }
+
+  public TypeSafeFieldEnd<? extends CriteriaContainer, com.antwerkz.jfokus.mongo.model.Address, java.lang.String> city() {
+    return new TypeSafeFieldEnd<>(query, query.criteria("city"));
+  }
+
+  public TypeSafeFieldEnd<? extends CriteriaContainer, com.antwerkz.jfokus.mongo.model.Address, org.bson.types.ObjectId> id() {
+    return new TypeSafeFieldEnd<>(query, query.criteria("id"));
+  }
+
+  public TypeSafeFieldEnd<? extends CriteriaContainer, com.antwerkz.jfokus.mongo.model.Address, java.lang.String> state() {
+    return new TypeSafeFieldEnd<>(query, query.criteria("state"));
+  }
+
+  public TypeSafeFieldEnd<? extends CriteriaContainer, com.antwerkz.jfokus.mongo.model.Address, java.lang.String> street() {
+    return new TypeSafeFieldEnd<>(query, query.criteria("street"));
+  }
+
+  public TypeSafeFieldEnd<? extends CriteriaContainer, com.antwerkz.jfokus.mongo.model.Address, java.lang.String> street2() {
+    return new TypeSafeFieldEnd<>(query, query.criteria("street2"));
+  }
+
+  public TypeSafeFieldEnd<? extends CriteriaContainer, com.antwerkz.jfokus.mongo.model.Address, java.lang.String> zip() {
+    return new TypeSafeFieldEnd<>(query, query.criteria("zip"));
+  }
+}
